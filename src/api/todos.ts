d5dd845a77fa85b727557a74aa997f4afc2c1591
Kyle Lawhorn <kylelawhorn@tinyhomeconsultingllc.com>
2022-01-04 13:47:20 -0500

    build out API

---
+++ src/api/todos.ts
@@ -0,0 +1,49 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
+
+interface Todo {
+    name: string,
+    date: string,
+    completed: boolean
+}
+
+export const getTodos = (
+    request: Request, 
+    handler: ResponseToolkit
+) => {
+    return {
+        "test": "123"
+    }
+}
+
+export const createTodo = (
+    request: Request,
+    handler: ResponseToolkit
+) => {
+    const todo = request.payload as Todo
+
+    return todo
+}
+
+export const updateTodo = (
+    request: Request,
+    handler: ResponseToolkit
+) => {
+    const todoId = request.params.todoId as number
+    const todo = request.payload as Todo
+
+    return {
+        todoId,
+        ...todo
+    }
+}
+
+export const deleteTodo = (
+    request: Request,
+    handler: ResponseToolkit
+) => {
+    const todoId = request.params.todoId as number
+
+    return {
+        todoId
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,55 @@
 import { Server } from "@hapi/hapi";
+import { createTodo, deleteTodo, getTodos, updateTodo } from "./api/todos";
+import Joi from 'joi'
+
+const todoPayload = Joi.object({
+    name: Joi.string().required(),
+    date: Joi.string().required(),
+    completed: Joi.boolean().required()
+})
+
+const todoParams = Joi.object({
+    todoId: Joi.number().required()
+})
 
 const routes = (server: Server) => {
     server.route({
         method: 'GET',
-        path: '/',
-        handler: (request, hapi) => {
-            return 'Hello World'
+        path: '/todos',
+        handler: getTodos
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/todos',
+        handler: createTodo,
+        options: {
+            validate: {
+                payload: todoPayload
+            }
+        }
+    });
+
+    server.route({
+        method: 'PUT',
+        path: '/todos/{todoId}',
+        handler: updateTodo,
+        options: {
+            validate: {
+                params: todoParams,
+                payload: todoPayload
+            }
+        }
+    });
+
+    server.route({
+        method: 'DELETE',
+        path: '/todos/{todoId}',
+        handler: deleteTodo,
+        options: {
+            validate: {
+                params: todoParams
+            }
         }
     });
 }
